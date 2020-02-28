4e587129b747e2fae7d47d60ecc4ec10e6140751
Feuerhamster <feuerhamster02@gmail.com>
2020-02-28 21:26:36 +0100

    First working version

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # better-rss
+Better RSS is a wrapper around [rss-parser](https://github.com/rbren/rss-parser) with auto-update, events and some useful features.
+
+## Installation
+Go to your node project and run 
+`npm install better-rss`
+
+## Usage
+```javascript
+const betterRSS = require('./main');
+const rss = new betterRSS({ updateInterval: 120000 });
+
+rss.feeds().add('https://some-url.to/my/rss/feed.xml');
+
+rss.on('newItem', (item, feed) => {
+    // do your stuff
+});
+```
+
+## Documentation
+
+### Class `new betterRSS(options)`
+- `options` | **object** | An object that will be given in the constructor
+    - `feeds` | **array** | An array with strings of your rss feeds
+    - `updateInterval` | **number** | A number that specify the interval of update requests in milliseconds
+    - `autoUpdate` | **bool** | This boolean defines whether automatic updates should be performed
+    
+### Methods
+### `feeds()`
+- `add(url)`
+    - `url` | **string** | The url to the rss feed
+    - **returns:** boolean
+        - *true* | Successful added
+        - *false* | Already in feeds
+- `get(parsed)`
+    - `parsed` | **bool** | Define if you want to get an array with only strings or an array with parsed url objects
+    - **returns:** Array
+- `remove(url)`
+    - `url` | **string** | The url to the rss feed
+        - **returns:** boolean
+            - *true* | Successful removed
+            - *false* | Not found
+- `has(url)`
+    - `url` | **string** | The url to the rss feed
+        - **returns:** boolean
+            - *true* | Found
+            - *false* | Not found
+- `set(urlArray)`
+    - **urlArray** | *Array* | Array with strings (your rss urls)
+    
+### `updater()`
+- `start()` | Start the auto updater
+- `stop()` | Stop the auto updater
+    - **returns:**
+        - *true* | Successful stopped
+        - *false* | Cant stopped because auto updater not running
+- `update()` | Update all rss feeds now
+
+### `on(event, callback)`
+- `event` | **string** | The event that you want to listen on
+- `callback([args])` | **function** | The callback function that will be triggered on an event
+
+### `fetchFeed(url)`  `fetchFeed(index)`
+- `url` | **string** | A url to a rss feed
+- `index` | **number** | Index of a feed in feeds
+- **returns:**
+    - *Promise*
+
+### `updateFeeds()`
+Updates all feeds. Please not use this. Use `updater().update()` instead.
+
+### Events
+- **updating** | Will be triggered when the auto updater updates all rss feeds
+    - No callback args
+- **error** | Triggered when an error occurs
+    - `err` | **object** | Error object in callback arg
+- **newItem** | Executed if a new item in a feed is available
+    - `item` | **object** | RSS item object
+    - `feed` | **object** | RSS feed object
---
+++ main.js
@@ -0,0 +1,182 @@
+class BetterRSS{
+
+	constructor(options) {
+
+		const events = require('events');
+		const rss = require('rss-parser');
+		this._url = require('url');
+
+		this._events = new events.EventEmitter();
+		this._rss = new rss();
+
+		this.updateInterval = options.updateInterval ? options.updateInterval : 120000;
+		this.feedLinks = options.feeds ? options.feeds : [];
+
+		this._currentItems = {};
+
+		this._updater = null;
+
+		this.updateFeeds(true);
+
+		// start auto update
+		if(options.autoUpdate !== false){
+			this.updater().start();
+		}
+
+	}
+
+	updateFeeds(){
+
+		// loop over all feed urls
+		for(let url of this.feedLinks){
+
+			// get _rss feed
+			this._rss.parseURL(url)
+				.then((feed) => {
+
+					/*
+					* Check if feed must be initialized.
+					* A feed is not initialized, if in this.currentItem is no array.
+					* If a feed is not initialized, the loop over items and event emitter will be ignored.
+					* */
+					if(typeof this._currentItems[feed.link] !== 'undefined'){
+
+						// loop over all items in feed
+						for(let item of feed.items){
+							// check if there is a new item
+							if(!this._currentItems[feed.link].includes(item.link)){
+								this._events.emit("newItem", item, feed);
+							}
+
+						}
+
+					}
+
+					/*
+					* Set _currentItems for this feed to array with links of all items
+					* This will be done every time to be up-to-date with all items
+					* */
+					this._currentItems[feed.link] = Array.from(feed.items, (el) => el.link);
+
+				})
+				.catch((err) => {
+					this._events.emit('error', err);
+				});
+
+		}
+
+	}
+
+	updater(){
+
+		return {
+			start: () => {
+
+				// check if updater is null. If not, clear it
+				if(this._updater !== null){
+					clearInterval(this._updater);
+				}
+
+				this._updater = setInterval(() => {
+
+					this._events.emit('updating');
+
+					this.updateFeeds();
+
+				}, this.updateInterval);
+
+			},
+			stop: () => {
+
+				if(this._updater !== null){
+
+					clearInterval(this._updater);
+					this._updater = null;
+
+					return true;
+
+				}else{
+					return false;
+				}
+
+			},
+			update: () => this.updateFeeds()
+		}
+
+	}
+
+	on(event, callback){
+
+		this._events.on(event, callback);
+
+	}
+
+	feeds(){
+		return {
+			add: (url) => {
+
+				if(!this.feedLinks.includes(url)){
+
+					this.feedLinks.push(url);
+					return true;
+
+				}else{
+					return false;
+				}
+
+			},
+			get: (parsed = false) => {
+
+				if(parsed){
+					return Array.from(this.feedLinks, (el) =>this._url.parse(el));
+				}else{
+					return this.feedLinks;
+				}
+
+			},
+			remove: (url) => {
+
+				let index = this.feedLinks.indexOf(url);
+
+				if(index > -1){
+
+					this.feedLinks.splice(index, 1);
+					return true;
+
+				}else{
+					return false;
+				}
+
+			},
+			has: (url) => {
+
+				return this.feedLinks.includes(url);
+
+			},
+			set: (urlArray) => {
+				this.feedLinks = urlArray;
+			}
+		}
+	}
+
+	fetchFeed(feed){
+
+		if(typeof feed === 'number'){
+
+			if(this.feedLinks[feed]){
+				return this._rss.parseURL(this.feedLinks[feed]);
+			}else{
+				return new Promise((resolve, reject)=>{
+					reject({ error: 'feed_index_not_found' });
+				});
+			}
+
+		}else{
+			return this._rss.parseURL(url);
+		}
+
+	}
+
+}
+
+module.exports = BetterRSS;
